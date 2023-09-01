6a65f728d5d4eec9ae7faea564104d23c164974a
brianchege2k <chegebrian84@gmail.com>
2023-09-01 17:19:32 +0300

    deployment change

--- reviws_app/settings.py
+++ reviws_app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0ka@#$b#cy8vr$1&k!c1+v@zzifevn15y_k-w!lrhid+#)f+cm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -129,4 +129,3 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-print("DATABASE_URL:", os.environ.get('DATABASE_URL'))
